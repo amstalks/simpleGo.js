a005d281d4ad431e332c093594160538494d15a1
Ahmed Almadhoob <ahmed.almadhoob@souqalmanama.com>
2023-07-10 17:20:31 +0300

    Added game play without logic

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function drawBoard(tableSize) {
-  //   console.info("Started drawing the board");
   const boardElement = document.getElementById("board");
   for (let i = 0; i < tableSize; i++) {
     let rowElement = document.createElement("tr");
@@ -8,15 +7,32 @@ function drawBoard(tableSize) {
     for (let j = 0; j < tableSize; j++) {
       let dataElement = document.createElement("td");
       dataElement.id = `d-${i}-${j}`;
-      dataElement.innerText = '.';
+      dataElement.innerText = ".";
       rowElement.appendChild(dataElement);
     }
   }
 }
 
 function play() {
-  // console.info("DOM fully loaded and parsed");
-  drawBoard(9);
+  const tableSize = 9;
+  drawBoard(tableSize);
+  currentTurn = "blue-box";
+
+  const box = document.querySelectorAll("td");
+  for (let i = 0; i < tableSize; i++) {
+    for (let j = 0; j < tableSize; j++) {
+      let dataElement = box[i * 9 + j];
+      dataElement.addEventListener("click", () => {
+        if (dataElement.innerText === "*") {
+          return;
+        } else {
+          dataElement.innerText = "*";
+          dataElement.className = currentTurn;
+          currentTurn = currentTurn === "blue-box" ? "red-box" : "blue-box";
+        }
+      });
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", play);
