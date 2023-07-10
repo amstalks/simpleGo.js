e6e9f07ad9ec842082735275055b4a28d720b009
Ahmed Almadhoob <ahmed.almadhoob@souqalmanama.com>
2023-07-10 15:49:40 +0300

    Added a function to draw the table

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+function drawBoard(tableSize) {
+  //   console.info("Started drawing the board");
+  const boardElement = document.getElementById("board");
+
+  for (let i = 0; i < tableSize; i++) {
+    let rowElement = document.createElement("tr");
+    rowElement.id = `r-${i}`;
+    boardElement.appendChild(rowElement);
+    for (let j = 0; j < tableSize; j++) {
+      let dataElement = document.createElement("td");
+      dataElement.id = `d-${i}-${j}`;
+      //   dataElement.innerText = "X";
+      rowElement.appendChild(dataElement);
+    }
+  }
+}
+
 function play() {
-    console.info("DOM fully loaded and parsed");
+  // console.info("DOM fully loaded and parsed");
+  drawBoard(9);
 }
 
 document.addEventListener("DOMContentLoaded", play);
